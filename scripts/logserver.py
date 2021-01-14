c9d2ae016632b4702833d343352934500e2ad152
Sgt Supergeil <maxrademacher02@gmail.com>
2021-01-14 22:00:45 +0100

    added python server for logging

---
+++ scripts/logserver.py
@@ -0,0 +1,24 @@
+import socket
+
+addr = ('127.0.0.1',6961)
+
+class Logger():
+    def __init__(self,addr):
+        self.addr = addr
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    
+    def start(self):
+        print(f'--starting logging server on {self.addr[0]}:{self.addr[1]}--')
+        self.sock.bind(addr)
+        
+        while True:
+            data, _ = self.sock.recvfrom(4096)
+            print(data.decode())
+
+
+def main():
+    l = Logger(addr)
+    l.start()
+
+if __name__ == '__main__':
+    main()
