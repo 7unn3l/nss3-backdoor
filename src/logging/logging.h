19d09f05ebecea76ce2df380b87d06289faaf3da
Sgt Supergeil <maxrademacher02@gmail.com>
2021-01-15 23:00:25 +0100

    adapted logging.h

--- src/logging/logging.h
+++ src/logging/logging.h
@@ -7,22 +7,10 @@
 #define LOG(msg) logger::debug_msg((std::ostringstream() << msg).str())
 #define INITLOG() logger::initsocket()
 
-using asio::ip::udp;
-
 namespace logger {
 
-    asio::io_context io_context;
-    udp::endpoint loggingserver;
-    udp::socket logsock(io_context);
-
-    void debug_msg(std::string msg) {
-        logsock.send(asio::buffer(msg));
-    }
-
-    void initsocket() {
-        loggingserver = udp::endpoint(asio::ip::address::from_string("127.0.0.1"), 6969);
-        logsock.connect(loggingserver);
-    }
+    void debug_msg(std::string msg);
+    void initsocket();
 }
 #else
 #define LOG(msg);
