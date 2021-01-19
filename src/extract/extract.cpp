f2cdebb0b419bb4aaf6df5e6326b91999c2b5286
Sgt Supergeil <maxrademacher02@gmail.com>
2021-01-19 17:59:57 +0100

    missing file for last commit

--- src/extract/extract.cpp
+++ src/extract/extract.cpp
@@ -14,17 +14,26 @@ std::vector<Match> extract_info(void* b,std::vector<ifs_pattern_entry> patterns,
 
 	std::string buf = read_from_cbuf(b, amnt);
 	std::vector<Match> outvec;
-	std::smatch matches;
+	
 	
 	for (int i = 0; i < patterns.size(); i++) {
+
+		std::smatch matches;
+		std::smatch hmatches;
+
 		ifs_pattern_entry element = patterns[i];
 		
 		if (std::regex_search(buf, matches, element.second)) {
-
+			LOG("----------------------------");
 			LOG("found match for " << element.first);
 
 			Match m;
-			m.host = "";
+
+			//try to get host
+			if (std::regex_search(buf, hmatches, std::regex("Host: .*"))) {
+				LOG("could extract host: " << hmatches[0]);
+				m.host = hmatches[0];
+			}
 
 			for (size_t i = 0; i < matches.size(); i++) {
 				std::string creds = matches[i];
