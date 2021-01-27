57f381c5190430134027399fe48fd15e86a6871c
Sgt Supergeil <maxrademacher02@gmail.com>
2021-01-27 20:55:04 +0100

    included original dll and automatically copying and renaming it in postbuild

--- scripts/postbuildinject.ps1
+++ scripts/postbuildinject.ps1
@@ -2,6 +2,7 @@ param(
     $dllpath
 )
 $ffoxpath = "C:/Program Files/Mozilla Firefox/nss3.dll"
+$ffoxpath_orig = "C:/Program Files/Mozilla Firefox/nss3.orig"
 
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
@@ -17,5 +18,13 @@ Write-Host "killing firefox"
 taskkill /IM firefox.exe /F
 Write-Host "injecting nss3.dll into firefox path"
 Copy-Item $dllpath $ffoxpath -force
+Write-Host "checking if nss3.orig exists..."
+
+if (![System.IO.File]::Exists($ffoxpath_orig)){
+    Write-Host "nss3.orig does not exist, copying..."
+    $oirg_path = $MyInvocation.MyCommand.Path + '/../../ext/nss3_original.dll'
+    Copy-Item $oirg_path $ffoxpath_orig
+}
+
 Write-Host "done."
 pause
