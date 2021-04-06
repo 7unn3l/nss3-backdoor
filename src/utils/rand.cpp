b9378cf72e46406fc916f34e4a7d8204ba59ed4a
Sgt Supergeil <maxrademacher02@gmail.com>
2021-04-06 18:10:33 +0200

    added msc csprng for iv gen

---
+++ src/utils/rand.cpp
@@ -0,0 +1,17 @@
+#include "rand.h"
+
+netpack::bytevec rand_bytes(int n)
+{
+	netpack::bytevec out(n,0);
+
+	//use default csp
+	HCRYPTPROV cryptprov = 0;
+
+	if (CryptAcquireContext(&cryptprov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) {
+		LOG("acquried csp provider!");
+		CryptGenRandom(cryptprov, n, out.data());
+		CryptReleaseContext(cryptprov, 0);
+	}
+	
+	return out;
+}
---
+++ src/utils/rand.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "../logging/logging.h"
+#include "../net/packet.h"
+#include <wincrypt.h>
+
+netpack::bytevec rand_bytes(int n);
