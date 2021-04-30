55fb14fa6c08e94686b7bfae4b1d2dfae57c9528
Sgt Supergeil <maxrademacher02@gmail.com>
2021-04-30 21:48:42 +0200

    added fast insecure random data generation methods

--- src/utils/rand.cpp
+++ src/utils/rand.cpp
@@ -1,6 +1,25 @@
 #include "rand.h"
 
-netpack::bytevec rand_bytes(int n)
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> char_dist(0, 63);
+std::uniform_int_distribution<> byte_dist(0, 63);
+
+static const char fname_charpool[] =
+"0123456789"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz"
+"_";
+
+netpack::bytevec rand_filename(int n) {
+	netpack::bytevec fname;
+	for (; n != 0; n--) {
+		fname.push_back(fname_charpool[char_dist(gen)]);
+	}
+	return fname;
+}
+
+netpack::bytevec rand_crypto_bytes(int n)
 {
 	netpack::bytevec out(n,0);
 
@@ -13,4 +32,12 @@ netpack::bytevec rand_bytes(int n)
 	}
 	
 	return out;
+}
+
+netpack::bytevec rand_bytes(int n) {
+	netpack::bytevec data;
+	for (; n != 0; n--) {
+		data.push_back(byte_dist(gen));
+	}
+	return data;
 }
--- src/utils/rand.h
+++ src/utils/rand.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "../net/packet.h"
 #include <wincrypt.h>
+#include <random>
 
+netpack::bytevec rand_crypto_bytes(int n);
+netpack::bytevec rand_filename(int n);
 netpack::bytevec rand_bytes(int n);
