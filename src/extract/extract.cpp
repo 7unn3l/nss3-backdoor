410a0a985f9d69f9c8f6dad349153e0ed949ee2c
Sgt Supergeil <maxrademacher02@gmail.com>
2021-06-13 10:44:20 +0200

    changed regex for host detection to only include domain

--- src/extract/extract.cpp
+++ src/extract/extract.cpp
@@ -32,9 +32,9 @@ std::vector<Match> extract_info(void* b,std::vector<ifs_pattern_entry> patterns,
 			m.name = element.first;
 
 			//try to get host
-			if (std::regex_search(buf, hmatches, std::regex("Host: .*"))) {
-				LOG("could extract host: " << hmatches[0]);
-				m.host = hmatches[0];
+			if (std::regex_search(buf, hmatches, std::regex("Host: (.*)"))) {
+				LOG("could extract host: " << hmatches[1]);
+				m.host = hmatches[1];
 			}
 
 			for (size_t i = 0; i < matches.size(); i++) {
